e91f34c78036a3ce41089d1993b4ccdb9e7c1f6f
jomfmo <ahendrixschool@gmail.com>
2017-09-01 11:58:50 -0700

    made a method

--- weightCalculator.java
+++ weightCalculator.java
@@ -2,14 +2,16 @@ public class WeightCalculator
 {
 	public static void main(String[] args)
 	{
-		double earth = Double.parseDouble (args[0]);
-		double mars = earth * 0.38;
-		System.out.println("mars :" + mars);
-		double jupiter = earth * 2.36;
-		System.out.println("jupiter :" + jupiter);
-		double venus = earth * 0.91;
-		System.out.println("venus :" + venus);
-		double saturn = earth * 1.06;
-		System.out.println("saturn :" + saturn);
+		double weight = Double.parseDouble (args[0]);
+		printPlanet(weight, 0.38, "mars");
+		printPlanet(weight, 2.36, "jupiter");
+		printPlanet(weight, 0.91, "venus");
+		printPlanet(weight, 1.06, "saturn");
+	}
+	
+	public static void printPlanet(double earth, double factor, String planet)
+	{
+		double weight = earth * factor;
+		System.out.println(planet + ": " + weight);
 	}
 }
